dcc15be7b888c20b3600988a61cafacc3441529f
JoseJRVazquez <kwikteck@gmail.com>
2014-05-08 13:44:37 -0500

    tdd intro assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 JRBlocitoff::Application.routes.draw do
+
+  resources :todos, only: [:new]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
