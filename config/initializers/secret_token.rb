2ec595f299faa7452f33107c2a56d9fa32ea3cbf
JoseJRVazquez <kwikteck@gmail.com>
2014-05-08 12:32:20 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JRBlocitoff::Application.config.secret_key_base = '50de6bc4078ef70013a0e0ea84172ed182ee387943cde2db8d28e0670284b8156c3d614b5b9848a9eb14fb1134485044cac81dbdffb741976f67a560fb8e8abf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JRBlocitoff::Application.config.session_store :cookie_store, key: '_JRBlocitoff_session'
