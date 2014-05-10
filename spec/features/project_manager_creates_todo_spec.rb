7cf888812f8a3eeb473f9073eafb7d382730b987
JoseJRVazquez <kwikteck@gmail.com>
2014-05-10 09:53:02 -0500

    Failure RSpec

--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -8,4 +8,14 @@ feature 'Project manager creates TODO' do
       expect ( page ).to have_content('Your new TODO was saved')
       expect ( page ).to have_content('Meet up with the team')
     end
+
+
+
+feature 'Project fails' do
+  scenario 'Fail' do
+    visit new_todo_path
+    fill_in 'Description', with ''
+    click_button 'Save'
+      expect ( page ).to have_content('No team meeting')
+    end
 end
